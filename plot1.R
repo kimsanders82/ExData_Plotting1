677c1037a24460f82ceb04f8680fb047a2dc26f7
Kim Sanders <ksanders@alfains.com>
2017-05-25 18:52:01 -0500

    Initial Commit for plot1.R and plot1.png

---
+++ plot1.R
@@ -0,0 +1,39 @@
+
+
+
+library(data.table)
+
+
+setwd("C:/Coursera/Week 1 Project")
+
+
+## Download and unzip file into working directory
+
+fileUrl = 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+if (!file.exists('./household_power_consumption.zip')){
+  download.file(fileUrl,'./household_power_consumption.zip', mode = 'wb')
+  unzip("./household_power_consumption.zip", exdir = getwd())
+}
+
+
+##### Read in .txt file
+
+myData <- read.table(paste0(getwd(),"/household_power_consumption.txt"), header=TRUE, sep=";", na.strings="?")
+
+
+## Filter data to only include dates Feb 1, 2007 and Feb 2, 2007 and set the time variable
+myData <- myData[myData$Date %in% c("1/2/2007","2/2/2007"),]
+SetTime <-strptime(paste(myData$Date, myData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+myData <- cbind(SetTime, myData)
+
+
+## Create Global Active Power plot and save to .png file
+
+png(paste0(getwd(),"/plot1.png"))
+
+hist(myData$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+
+dev.off()
+
+
+
